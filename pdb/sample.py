3913c9809cd66b53a7530ce62c41c53ae74d614d
takahi-i <takahikoito@gmail.com>
2020-02-02 23:41:29 +0900

    Add sample for pdb

---
+++ pdb/sample.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# copied from http://racchai.hatenablog.com/entry/2016/05/30/070000
+import sys
+
+def log(args):
+    print(args)
+
+def main(args):
+    log(args)
+
+import pdb;pdb.set_trace()
+main(sys.argv)
