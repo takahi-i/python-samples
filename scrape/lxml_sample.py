173cb82ed67b8440e086958aa9ae558295d65f5f
takahi-i <takahikoito@gmail.com>
2020-05-31 22:36:16 +0900

    Add a sample of lxml

--- scrape/lxml_sample.py
+++ scrape/lxml_sample.py
@@ -4,5 +4,7 @@ import requests
 url = 'http://unigram.org/index.html'
 r = requests.get(url) 
 html = lxml.html.fromstring(r.text)
-title = html.xpath('//*[@id="content"]/div[2]/div[1]/h4/a[1]/text()') # xpath copied from unigram.org
-print(title)
+
+for title in html.cssselect(".product > .title > a"):
+    if title.text:
+        print(title.text)
