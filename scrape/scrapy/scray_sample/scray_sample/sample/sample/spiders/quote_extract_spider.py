95c476a62d847316bf39199f4c3d87dfac960d23
takahi-i <takahikoito@gmail.com>
2020-06-21 23:20:49 +0900

    Add spiders

---
+++ scrape/scrapy/scray_sample/scray_sample/sample/sample/spiders/quote_extract_spider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quote_extract"
+    start_urls = [ 
+       'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
